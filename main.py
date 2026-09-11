08174116de9c9b28ede3bdfd32c56c44d8827d21
MMonterrosaSV <MMonterrosaSV@gmail.com>
2026-09-11 14:22:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -47,12 +47,12 @@ def extract_m3u8(url: str, headless: bool = True):
 
 
 channels = {
-    "Dazn La Liga": "https://tvnow247.top/embed/dazn-laliga/",
-    "Dazn La Liga 2": "https://cdnlivetv.is/api/v1/channels/player/?name=DAZN%20LaLiga%202&code=es&user=cdnlivetv&plan=free",
+    "Dazn La Liga 1": "https://tvnow247.top/embed/dazn-laliga/",
+    "Dazn La Liga 2 BACK UP STREAMSPORTS99": "https://cdnlivetv.is/api/v1/channels/player/?name=DAZN%20LaLiga%202&code=es&user=cdnlivetv&plan=free",
     "M+ LA LIGA 1": "https://tvnow247.top/embed/movistar-laliga/",
-    "M+ LA LIGA 1 (ntv)": "https://ntv.cx/channel/falcon/LALIGA-ES",
-    "M+ CHAMPIONS LEAGUE 1 (tvnow247)": "https://tvnow247.top/embed/movistar-liga-de-campeones/",
-    "M+ CHAMPIONS LEAGUE 1 (ntv)": "https://ntv.cx/channel/falcon/MOVISTAR-LIGA-DE-CAMPEONES",
+    "M+ LA LIGA 1 BACK UP NTV": "https://ntv.cx/channel/falcon/LALIGA-ES",
+    "M+ CHAMPIONS LEAGUE 1": "https://tvnow247.top/embed/movistar-liga-de-campeones/",
+    "M+ CHAMPIONS LEAGUE 1 BACK UP NTV": "https://ntv.cx/channel/falcon/MOVISTAR-LIGA-DE-CAMPEONES",
     "M+ DEPORTES 1": "https://tvnow247.top/embed/movistar-deportes-4",
     "M+ DEPORTES 2": "https://tvnow247.top/embed/movistar-deportes-2/",
     "M+": "https://tvnow247.top/embed/movistar-supercopa-de-espana/",
