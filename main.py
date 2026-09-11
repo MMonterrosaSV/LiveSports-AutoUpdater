fd3266463bb2fb5aef89d924a00f8801c9a87674
MMonterrosaSV <MMonterrosaSV@gmail.com>
2026-09-11 20:20:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,6 @@ def extract_m3u8(url: str, headless: bool = True):
 channels = {
     "DAZN LA LIGA 1": "https://tvnow247.top/embed/dazn-laliga/",
     "M+ LA LIGA 1": "https://tvnow247.top/embed/movistar-laliga/",
-    "M+ LA LIGA 1 BACKUP": "https://huhu.to/watch?live=2428779244f1f77edafe50",
     "M+ CHAMPIONS LEAGUE 1": "https://tvnow247.top/embed/movistar-liga-de-campeones/",
     "M+ DEPORTES 1": "https://tvnow247.top/embed/movistar-deportes-4",
     "M+ DEPORTES 2": "https://tvnow247.top/embed/movistar-deportes-2/",
@@ -62,7 +61,6 @@ channels = {
     "TNT SPORTS 3": "https://tvnow247.top/watch/tnt-sports-3/",
     "SKY SPORTS PREMIER LEAGUE": "https://tvnow247.top/watch/sky-sports-premier-league/",
     "SKY SPORTS FOOTBALL": "https://tvnow247.top/watch/sky-sports-football/",
-    "SKY SPORTS FOOTBALL BACKUP": "https://huhu.to/watch?live=12588537487978d6179a2d",
     "SKY SPORTS PLUS": "https://tvnow247.top/watch/sky-sports-plus/",
     "SKY SPORTS MAIN EVENT": "https://tvnow247.top/watch/sky-sports-main-event/",
 }
