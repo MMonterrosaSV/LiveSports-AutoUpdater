35c5ec6ba85ffa471f0e96f1790e666d635a0c73
MMonterrosaSV <MMonterrosaSV@gmail.com>
2026-09-11 15:52:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -48,11 +48,8 @@ def extract_m3u8(url: str, headless: bool = True):
 
 channels = {
     "Dazn La Liga 1": "https://tvnow247.top/embed/dazn-laliga/",
-    "Dazn La Liga 2 STREAMSPORTS99": "https://cdnlivetv.is/api/v1/channels/player/?name=DAZN%20LaLiga%202&code=es&user=cdnlivetv&plan=free",
     "M+ LA LIGA 1": "https://tvnow247.top/embed/movistar-laliga/",
-    "M+ LA LIGA 1 BACKUP NTV": "https://ntv.cx/channel/falcon/LALIGA-ES",
     "M+ CHAMPIONS LEAGUE 1": "https://tvnow247.top/embed/movistar-liga-de-campeones/",
-    "M+ CHAMPIONS LEAGUE 1 BACKUP NTV": "https://ntv.cx/channel/falcon/MOVISTAR-LIGA-DE-CAMPEONES",
     "M+ DEPORTES 1": "https://tvnow247.top/embed/movistar-deportes-4",
     "M+ DEPORTES 2": "https://tvnow247.top/embed/movistar-deportes-2/",
     "M+": "https://tvnow247.top/embed/movistar-supercopa-de-espana/",
