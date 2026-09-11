ace7d5fe875162861cc3503ee974dc4dcedd96b4
MMonterrosaSV <MMonterrosaSV@gmail.com>
2026-09-11 17:12:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def extract_m3u8(url: str, headless: bool = True):
 
 
 channels = {
-    "Dazn La Liga 1": "https://tvnow247.top/embed/dazn-laliga/",
+    "DAZN LA LIGA 1": "https://tvnow247.top/embed/dazn-laliga/",
     "M+ LA LIGA 1": "https://tvnow247.top/embed/movistar-laliga/",
     "M+ LA LIGA 1 BACKUP": "https://huhu.to/watch?live=2428779244f1f77edafe50",
     "M+ CHAMPIONS LEAGUE 1": "https://tvnow247.top/embed/movistar-liga-de-campeones/",
